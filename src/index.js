c4dcb99705b3ae5ed1f8b4bbd3c307e81db8a2c2
bclauson <clauson.branden@gmail.com>
2022-01-15 12:32:43 -0800

    add some extra set up html code

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $('#currencyReady').click(function() {
+    let value = $('#amount').val();
+    let currency = $('#currency').val();  
+    $('.showCurrency').text(`The currency is ${value}`);
+    $('.showAmount').text(`The ammount is ${currency}`);
+  });
+});
