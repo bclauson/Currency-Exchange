709bf1333554e2cc5a3aff77bea8f69ba44246aa
bclauson <clauson.branden@gmail.com>
2022-01-15 12:47:07 -0800

    add CurrencyExchange api call code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './CurrencyExchange.js'
+
 
 $(document).ready(function() {
   $('#currencyReady').click(function() {
     let value = $('#amount').val();
     let currency = $('#currency').val();  
-    $('.showCurrency').text(`The currency is ${value}`);
-    $('.showAmount').text(`The ammount is ${currency}`);
+    $('.showCurrency').text(`The currency is ${currency}`);
+    $('.showAmount').text(`The ammount is ${value}`);
   });
 });
