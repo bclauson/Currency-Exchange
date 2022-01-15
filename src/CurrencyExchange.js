e171cdf70a92cb7cc0b78c2e39e4666393df9a34
bclauson <clauson.branden@gmail.com>
2022-01-15 14:14:39 -0800

    add more UI for user clarity, removed rounding for accuracy

--- src/CurrencyExchange.js
+++ src/CurrencyExchange.js
@@ -1,12 +1,10 @@
 export default class CurrencyExchange {
-  static getCurrency(currencyType, amount) {
+  static getCurrency() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
-        console.log(amount);
-        console.log(currencyType);
         return response.json();
       })
       .catch(function(error) {
--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './CurrencyExchange.js'
 
-function getElements(response, currency) {
+function getElements(response, currency, value) {
   if (response.result) {
     let conversionVal
+    let convertedVal
     if (currency in response.conversion_rates) {
       conversionVal = response.conversion_rates[currency];
-    console.log(typeof response.conversion_rates);
-    $('.showCurrency').text(`The API call gives ${conversionVal}`);
-    $('.showAmount').text(`The currency is ${currency}`);
+      convertedVal = conversionVal * value;
+      //convertedVal = convertedVal.toFixed(2);
+      $('.showAmount').text(`From ${value} USD to ${currency} the exchange results in ${convertedVal}`);
     } else {
-      $('.showCurrency').text(`There was an error: ISO 4217 currency code not found (code must be in capitals)`);
+      $('.showAmount').text(`There was an error: ISO 4217 currency code not found (code must be in capitals)`);
     }
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
@@ -23,11 +24,13 @@ function getElements(response, currency) {
 $(document).ready(function() {
   $('#currencyReady').click(function() {
     let value = $('#amount').val();
-    let currency = $('#currency').val();  
+    let currency = $('#currency').val(); 
+    $('#currency').val("");
+    $('#amount').val("");
     $('.showAmount').text(`The ammount is ${value}`);
-    CurrencyExchange.getCurrency(currency, value)
+    CurrencyExchange.getCurrency()
       .then(function(response) {
-        getElements(response,currency);
+        getElements(response, currency, value);
       });
 
   });
