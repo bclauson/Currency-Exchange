ffd03fed608f7be8eb5c68112e8e5832bb153766
bclauson <clauson.branden@gmail.com>
2022-01-15 13:55:53 -0800

    change code to recognize user input for ISO currency codes instead of drop down menu

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ function getElements(response, currency) {
     let conversionVal
     if (currency in response.conversion_rates) {
       conversionVal = response.conversion_rates[currency];
-    }
     console.log(typeof response.conversion_rates);
     $('.showCurrency').text(`The API call gives ${conversionVal}`);
     $('.showAmount').text(`The currency is ${currency}`);
-    
+    } else {
+      $('.showCurrency').text(`There was an error: ISO 4217 currency code not found (code must be in capitals)`);
+    }
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
@@ -23,7 +24,6 @@ $(document).ready(function() {
   $('#currencyReady').click(function() {
     let value = $('#amount').val();
     let currency = $('#currency').val();  
-    $('.showCurrency').text(`The currency is ${currency}`);
     $('.showAmount').text(`The ammount is ${value}`);
     CurrencyExchange.getCurrency(currency, value)
       .then(function(response) {
