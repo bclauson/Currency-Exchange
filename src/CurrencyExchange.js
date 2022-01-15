2206dd6d159f4652c36835faf78f8b10b0c9d743
bclauson <clauson.branden@gmail.com>
2022-01-15 13:47:40 -0800

    add js code to recognize API call and use input to navigate path

--- src/CurrencyExchange.js
+++ src/CurrencyExchange.js
@@ -1,14 +1,16 @@
 export default class CurrencyExchange {
-  static getCurrency(currencyType) {
+  static getCurrency(currencyType, amount) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
+        console.log(amount);
+        console.log(currencyType);
         return response.json();
       })
       .catch(function(error) {
         return error;
-      })
+      });
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './CurrencyExchange.js'
 
+function getElements(response, currency) {
+  if (response.result) {
+    let conversionVal
+    if (currency in response.conversion_rates) {
+      conversionVal = response.conversion_rates[currency];
+    }
+    console.log(typeof response.conversion_rates);
+    $('.showCurrency').text(`The API call gives ${conversionVal}`);
+    $('.showAmount').text(`The currency is ${currency}`);
+    
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
 $(document).ready(function() {
   $('#currencyReady').click(function() {
@@ -11,5 +25,10 @@ $(document).ready(function() {
     let currency = $('#currency').val();  
     $('.showCurrency').text(`The currency is ${currency}`);
     $('.showAmount').text(`The ammount is ${value}`);
+    CurrencyExchange.getCurrency(currency, value)
+      .then(function(response) {
+        getElements(response,currency);
+      });
+
   });
 });
